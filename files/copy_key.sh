dc1c50d20697e1ac528dbe00f0af3a14a2e37dab
E Veal <eveal@splunk.com>
2022-02-15 01:19:16 +0000

    14 FEB 2022

---
+++ files/copy_key.sh
@@ -0,0 +1,6 @@
+sshpass -p splunk3du ssh-copy-id -o stricthostkeychecking=no 10.0.4.12 <<<y
+sshpass -p splunk3du ssh-copy-id -o stricthostkeychecking=no 10.0.4.14 <<<y
+sshpass -p splunk3du ssh-copy-id -o stricthostkeychecking=no 10.0.4.17 <<<y
+sshpass -p splunk3du ssh-copy-id -o stricthostkeychecking=no 10.0.4.18 <<<y
+sshpass -p splunk3du ssh-copy-id -o stricthostkeychecking=no 10.0.4.21 <<<y
+sshpass -p splunk3du ssh-copy-id -o stricthostkeychecking=no 10.0.4.23 <<<y
